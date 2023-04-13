8f31486a3d579c7e587ab8c0ed963f33523a5bd7
Brandon Haupt <haupt.brandon@gmail.com>
2023-04-13 18:33:40 -0500

    implimenting mobile support

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -3,12 +3,47 @@ import HeaderCloud from "./img/Header-clouds.png"
 import PlayStar from "./img/Play_Now_Star.svg"
 import MiniStar from './img/Mini_Star.svg'
 
+import { useState } from "react"
+
+
+// const opened = document.getElementById("active");
+
+
 export default function Header(){
+
+    // const opened = useRef();
+
+    // console.log(opened)
+
+    const [isActive, setIsActive] = useState(false);
+
+    
+    // function activeNarBar() {
+    //     console.log("start")
+        
+    //     // if (opened === "active") {
+    //     //     opened.current.classList.remove("active");
+    //     //     console.log("removed active class");
+    //     // } else {
+    //     //     opened.current.classList.add("active");
+    //     //     console.log("added active class");
+    //     // }
+  
+    //     console.log("end")
+    // } 
+
     return(
         <header>
             <img className='site-logo' src={Logo} alt='toontowns event horizon logo'/>
-            <div className='nav'>
+            <div className='nav'  id={isActive ? 'active' : ''} onClick={() => setIsActive(!isActive)}>
+                <span className="hamburger-lines">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </span>
+
                 <img className='header-clouds' src={HeaderCloud} alt='space clouds'/>
+
                 <nav>
                     <a className='miniStar' href="#">
                         <img src={MiniStar} />
@@ -38,9 +73,15 @@ export default function Header(){
                         <img src={MiniStar} alt='a mini star'/>
                         Help
                     </a>
+
+
+                    
+
                 </nav>
             </div>
             
+
+           
         </header>
     )
 }
