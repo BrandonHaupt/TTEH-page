d8e365ed3877c49e450fc70853e81bd46245dc8f
Brandon Haupt <haupt.brandon@gmail.com>
2023-04-14 21:39:43 -0500

    added more sections to body to get general layout

--- src/components/pages/Body.js
+++ src/components/pages/Body.js
@@ -21,6 +21,51 @@ export default function Body(){
 
                 <img className='clouds2' src={Clouds2} alt="Clouds"/>
             </section>
+
+            <section className="adventure">
+                <h1>ADVENTURE SECTION</h1>
+            </section>
+
+            <section className='CloudContainer'>
+                <section className='container'>
+                    <section className='WordContainer'>
+                        <h1>What's The Big Idea Behind Event Horizon?</h1>
+                        <p>Toontown: Event Horizon is a Massively Multiplayer Online fan game that seeks to explore the un-ventured vision of Disneys Toontown Online. As a non-profit project, we will NEVER ask you to spend a DIME!</p>
+
+                        <p>With heavy space themes, shoot for the stars as you create your Toon and fight against the evil robot “Cogs” threatening to take over the galaxy!</p>
+
+                        <p>Are YOU Toon Enough!?</p>
+                    </section>
+                    
+                    <img className='ToonFight1' src={ToonFight1} alt="Toon fighting a cog"/>
+
+                </section>
+
+                <img className='clouds2' src={Clouds2} alt="Clouds"/>
+            </section>
+
+            <section className="adventure">
+                <h1>BLAST OFF SECTION</h1>
+            </section>
+
+            <section className='JoinTeamContianer'>
+                <section className='container'>
+                    <section className='WordContainer'>
+                        <h1>What's The Big Idea Behind Event Horizon?</h1>
+                        <p>Toontown: Event Horizon is a Massively Multiplayer Online fan game that seeks to explore the un-ventured vision of Disneys Toontown Online. As a non-profit project, we will NEVER ask you to spend a DIME!</p>
+
+                        <p>With heavy space themes, shoot for the stars as you create your Toon and fight against the evil robot “Cogs” threatening to take over the galaxy!</p>
+
+                        <p>Are YOU Toon Enough!?</p>
+                    </section>
+                    
+                    <img className='ToonFight1' src={ToonFight1} alt="Toon fighting a cog"/>
+
+                </section>
+
+                <img className='clouds2' src={Clouds2} alt="Clouds"/>
+            </section>
+
         </main>
     )
 }
